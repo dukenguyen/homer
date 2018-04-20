3addd157d56a235079a912c75326303bc25dad0d
dukenguyen <duke@sensemetrics.com>
2018-04-19 21:27:05 -0700

    c

---
+++ core/src/main/java/com/sensemetrics/AppCore.java
@@ -0,0 +1,17 @@
+package com.sensemetrics;
+
+////
+//
+//
+//
+/**
+ * Hello world!
+ *
+ */
+public class AppCore 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
