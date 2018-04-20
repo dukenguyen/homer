68f615639ce46e7b89d9ef092fc56565b1f01dcc
dukenguyen <duke@sensemetrics.com>
2018-04-19 19:40:25 -0700

    c

--- app/src/main/java/com/sensemetrics/App.java
+++ app/src/main/java/com/sensemetrics/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println(com.sensemetrics.Core.x);
         System.out.println( "Hello World!" );
     }
 }
